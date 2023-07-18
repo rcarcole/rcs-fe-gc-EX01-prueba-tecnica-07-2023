4acaf7b3d21d152b9541ebd89fea9ced6e0a7af6
rcarcole <rcarcole@hotmail.com>
2023-07-18 19:40:55 +0200

    hecho about

--- src/app/utils/navbar/navbar.component.ts
+++ src/app/utils/navbar/navbar.component.ts
@@ -1,29 +1,18 @@
 import { Component } from '@angular/core';
-// import { NgbModal, NgbTypeaheadConfig, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
-import { Observable, debounceTime, distinctUntilChanged, map } from 'rxjs';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  activeNavItem: number = 1;
+	activeNavItem: number = 1;
 
-  setActiveNavItem(navItem: number) {
-    this.activeNavItem = navItem;}
-  public model: any;
-
-	constructor() {
-		
+	setActiveNavItem(navItem: number) {
+		this.activeNavItem = navItem;
 	}
+	public model: any;
 
-	// search = (text$: Observable<string>) =>
-	// 	text$.pipe(
-	// 		debounceTime(200),
-	// 		distinctUntilChanged(),
-	// 		map((term) =>
-	// 			term.length < 2 ? [] : characters.splice(0, 10),
-	// 		),
-	// 	);
+	constructor() {}
+
 }
